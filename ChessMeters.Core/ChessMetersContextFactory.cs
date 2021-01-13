4035bcd94f06cfbfd93da339705870963c7d8253
Robert Hajbok <hajbok.robert@gmail.com>
2021-01-13 19:56:47 +0000

    Set new root password for MySQL and enable dotnet ef database update from Core project

---
+++ ChessMeters.Core/ChessMetersContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace ChessMeters.Core
+{
+    public class ChessMetersContextFactory : IDesignTimeDbContextFactory<ChessMetersContext>
+    {
+        public ChessMetersContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+            string mySqlConnectionString = configuration.GetConnectionString("DefaultConnection");
+            var optionsBuilder = new DbContextOptionsBuilder<ChessMetersContext>();
+            optionsBuilder.UseMySql(mySqlConnectionString, ServerVersion.AutoDetect(mySqlConnectionString));
+
+            return new ChessMetersContext(optionsBuilder.Options);
+        }
+    }
+}
