aefe87dd68d4b8b64628931b7e12786aaa014114
Robert Hajbok <hajbok.robert@gmail.com>
2021-01-14 22:35:43 +0000

    Add chess board to home page

--- ChessMeters.Web/ClientApp/src/app/app.module.ts
+++ ChessMeters.Web/ClientApp/src/app/app.module.ts
@@ -3,15 +3,16 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { NgxChessBoardModule } from 'ngx-chess-board';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { ApiAuthorizationModule } from 'src/api-authorization/api-authorization.module';
-import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
-import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
+import { ApiAuthorizationModule } from '../api-authorization/api-authorization.module';
+import { AuthorizeGuard } from '../api-authorization/authorize.guard';
+import { AuthorizeInterceptor } from '../api-authorization/authorize.interceptor';
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import { AuthorizeInterceptor } from 'src/api-authorization/authorize.intercepto
     HttpClientModule,
     FormsModule,
     ApiAuthorizationModule,
+    NgxChessBoardModule.forRoot(),
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
