92dbf7f7a2f960d80df3e3f8b94136a222b58fee
Robert Hajbok <hajbok.robert@gmail.com>
2021-01-16 21:38:16 +0000

    Improve UI a bit and add some functionalities to the chess board

--- ChessMeters.Web/ClientApp/src/app/home/home.component.ts
+++ ChessMeters.Web/ClientApp/src/app/home/home.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
+  templateUrl: './home.component.html'
 })
 export class HomeComponent {
+  public analyzeGame(): void {
+    alert('Not implemented yet');
+  }
 }
