cf0dbff9dca4708a002de90077e3120981f22dac
Robert Hajbok <hajbok.robert@gmail.com>
2021-01-18 20:58:10 +0000

    Scaffold identity Razor pages and customize for this project

---
+++ ChessMeters.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ChessMeters.Core.Database;
+using ChessMeters.Core.Entities;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ChessMeters.Web.Areas.Identity.IdentityHostingStartup))]
+namespace ChessMeters.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
