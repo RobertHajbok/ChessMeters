053f797b6ba319dd9140fa182c765b26cf04f7b1
Claudiu Oprea <claudiu.iulian3@gmail.com>
2021-01-23 22:58:26 +0000

    Deploy script: fix prod url

--- pull_linux.sh
+++ pull_linux.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+date
+PULL=`git pull`
+
+if [ "$PULL" = "Already up to date." ]
+then
+  echo "Nothing to update"
+  echo $PULL
+  exit 0
+fi
+
+kill -9 $(ps ax | grep dotnet | grep urls | awk -F ' ' '{ print $1}')
+
 git pull
 
 # database
@@ -7,5 +19,10 @@ cd /root/ChessMeters/ChessMeters.Core/
 /root/.dotnet/tools/dotnet-ef database update
 
 # npm install
-cd /root/ChessMeters/ChessMeters.Web/ClientApp
+cd /root/ChessMeters/ChessMeters.Web/ClientApp/
 npm install
+
+# run
+cd /root/ChessMeters/ChessMeters.Web/
+# dotnet run --urls "https://www.claudiu.chessmeters.com/" > /dev/null 2>&1 &
+dotnet run --urls "https://localhost:5000" > /dev/null 2>&1 &
